4b06c154289cbc835b2709cff10bda45739731aa
Naphat <Naphat@MSI>
2021-03-23 20:30:20 +0700

    Function that generates Bin Edges

--- generateBins.m
+++ generateBins.m
@@ -1,6 +1,8 @@
 function edgeList = generateBins(commonMZ, maxPeaks, tolerance, method)
-    if (checkTolerance(commonMZ,tolerance)) && (size(commonMZ) <= maxPeaks )
+    if (checkTolerance(commonMZ,tolerance)) & (size(commonMZ) <= maxPeaks )
     	edgeList = []; 
+    elseif (checkTolerance(commonMZ,tolerance)) &  (method=="Relaxed")
+        edgeList = []; 
     else
         edgeList = [];
         difList =  diff(commonMZ);
@@ -13,23 +15,9 @@ function edgeList = generateBins(commonMZ, maxPeaks, tolerance, method)
         leftEdgeList = generateBins(leftSubList,maxPeaks, tolerance, method);
         rightEdgeList = generateBins(rightSubList,maxPeaks, tolerance, method);
         
-        %for i = 1:size(leftEdgeList)
-            %edgeList(end+1) = leftEdgeList(i);
-        %end
-        %for j = 1:size(rightEdgeList)
-            %edgeList(end+1) = rightEdgeList(j);
-        %end
         edgeList = cat(2,edgeList,leftEdgeList);
         edgeList = cat(2,edgeList,rightEdgeList);
         edgeList = sort(edgeList);
-        
-        hAxes = axes('NextPlot','add',...           %# Add subsequent plots to the axes,
-             'DataAspectRatio',[1 1 1],...  %#   match the scaling of each axis,
-             'XLim',[10 40],...               %#   set the x axis limit,
-             'YLim',[0 eps],...             %#   set the y axis limit (tiny!),
-             'Color','none');               %#   and don't use a background color
-        plot(commonMZ,0,'r*','MarkerSize',10);  %# Plot data set 1
-        plot(edgeList,0,'b.','MarkerSize',10);  %# Plot data set 2
     end
 end
 
@@ -43,4 +31,3 @@ function isBin = checkTolerance(Peaklist,tolerance)
         end
     end
 end
-
