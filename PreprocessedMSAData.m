21c41a7d50710dfdb9d113adb609d30cb803d759
Naphat <Naphat@MSI>
2021-04-08 18:07:47 +0700

    Preproc

---
+++ PreprocessedMSAData.m
@@ -0,0 +1,13 @@
+classdef PreprocessedMSAData
+    
+    properties
+        PreprocessedSpectra
+        MinPosition %min pos to divide segment
+        SegmentLag %segment lag
+        ShiftedSegment %shifted segment
+    end
+    
+    methods
+    end
+end
+
